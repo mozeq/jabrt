fbebb53b1da9d8c89412013fe9a9d832f14777c0
Jiri Moskovcak <jmoskovc@redhat.com>
2013-09-06 13:43:07 +0200

    removed unnecessary comments

--- src/test/java/com/redhat/abrt/JABRTTest.java
+++ src/test/java/com/redhat/abrt/JABRTTest.java
@@ -3,15 +3,10 @@ package com.redhat.abrt;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-/**
- * Unit test for simple App.
- */
+
 @RunWith(JUnit4.class)
 public class JABRTTest
 {
-    /**
-     * Rigourous Test :-)
-     */
     @Test
     public void testAdd()
     {
